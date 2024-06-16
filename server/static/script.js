63a8c9451045196962369b19a083a13a3fa00c84
JongwooLee <jongwoo328@gmail.com>
2024-06-16 15:38:59 +0900

    불필요한 코드 삭제

--- server/static/script.js
+++ server/static/script.js
@@ -1,6 +1,5 @@
 const boardContainer = document.getElementById('board-container');
 const consoleOutput = document.getElementById('console');
-const recordOutput = document.getElementById('recordOutput');
 const size = 15;
 const letters = 'ABCDEFGHIJKLMNO';
 let turnCount = 0;
@@ -28,42 +27,8 @@ for (let x = 0; x < size; x++) {
         cell.classList.add('cell');
         cell.dataset.x = x;
         cell.dataset.y = y;
-        cell.addEventListener('click', handleClick);
         boardContainer.appendChild(cell);
     }
-}
-
-function handleClick(event) {
-    const x = event.target.dataset.x;
-    const y = event.target.dataset.y;
-
-    // 이미 돌이 있는지 확인
-    if (event.target.querySelector('.stone')) {
-        return;
-    }
-
-    // 턴 수 증가
-    turnCount++;
-
-    // 현재 플레이어 결정
-    const currentPlayer = (turnCount % 2 === 1) ? 'black' : 'white';
-
-    // 기보 형식
-    const moveFormat = letters[parseInt(x) - 1] + y;
-
-
-    const recordText = `record.add(player=${currentPlayer == 'black' ? 'test_player' : 'opponent_player'}, x=${x}, y=${y}, valid=True, reason="")\n`;
-    recordOutput.value += recordText;
-    consoleOutput.value += recordText;
-
-    // 바둑돌 생성
-    const stone = document.createElement('div');
-    stone.classList.add('stone');
-    stone.classList.add(currentPlayer);
-    event.target.appendChild(stone);
-
-    // 히스토리에 현재 상태 저장
-    history.push({ x, y, stone });
 }
 
 const appendLog = (log) => {
